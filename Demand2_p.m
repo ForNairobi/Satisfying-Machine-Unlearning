11e5dd0a2eefb19476328dd5bff80b32ea6e3d33
ForNairobi <91246140+ForNairobi@users.noreply.github.com>
2023-12-03 17:04:34 +0800

    Add files via upload

--- Demand2_p.m
+++ Demand2_p.m
@@ -31,6 +31,7 @@ set(gca, 'FontSize', 15, 'FontName', 'Times')
 dataline5 = plot(x, p_14, '-^', 'LineWidth', 2);
 set(gca, 'FontSize', 15, 'FontName', 'Times')
 
+
 grid on
 
 
@@ -39,5 +40,32 @@ xlabel('Iteration')
 ylabel('Price ($)')
 
 legend({'\kappa=0.6', '\kappa=0.8','\kappa=1.0', '\kappa=1.2', '\kappa=1.4'}, 'location', 'best');
+
+axes('position',[.65 .45 .25 .25]);
+box on
+
+p06 = [126.398, 126.398];
+p08 = [127.745, 127.745];
+p10 = [128.569, 128.569];
+p12 = [129.043, 129.043];
+p14 = [129.406, 129.406];
+
+sub_index = (x <= 10) & (x >= 9);
+
+line06 = plot(x(sub_index), p06, '-square', 'LineWidth', 2);
+set(gca, 'FontSize', 15, 'FontName', 'Times')
+hold on
+line08 = plot(x(sub_index), p08, '-', 'LineWidth', 2);
+set(gca, 'FontSize', 15, 'FontName', 'Times')
+line10 = plot(x(sub_index), p10, '--', 'LineWidth', 2);
+set(gca, 'FontSize', 15, 'FontName', 'Times')
+line12 = plot(x(sub_index), p12, '-o', 'LineWidth', 2);
+set(gca, 'FontSize', 15, 'FontName', 'Times')
+line14 = plot(x(sub_index), p14, '-^', 'LineWidth', 2);
+set(gca, 'FontSize', 15, 'FontName', 'Times')
+hold off
+
+axis tight
+grid on
 
 print('demand2_p.jpg', '-djpeg')
